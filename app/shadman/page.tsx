63a96c5c28e20536ee35151d1cf4b99c12714caa
ShadmaN1028 <shadmansaleheen0928@gmail.com>
2024-11-02 14:09:48 +0600

    initial

---
+++ app/shadman/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Button } from "@/components/ui/button"
+
+const shadman = () => {
+  return (
+    <div>shadman
+        <Button variant="destructive">Button</Button>
+
+    </div>
+
+  )
+}
+
+export default shadman
---
+++ next.config.mjs
@@ -0,0 +1,14 @@
+/** @type {import('next').NextConfig} */
+
+const nextConfig = {
+  images: {
+    domains: [
+      "cdn.dummyjson.com",
+      "localhost",
+      "hips.hearstapps.com",
+      "ui-avatars.com",
+    ],
+  },
+};
+
+export default nextConfig;
