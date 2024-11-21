298bf80f5894c878e7d539a07808055b2a9cd3cf
Raian Ruku <raianruku21@gmail.com>
2024-11-22 01:31:18 +0600

    minor

--- api/v1/owner/notifications/queries/owner-notifications.js
+++ api/v1/owner/notifications/queries/owner-notifications.js
@@ -6,7 +6,7 @@ const queries = {
       INSERT INTO ${table_name} (user_id, owner_id, description, status, created_by, creation_date, last_updated_by, last_update_date, change_number) 
        VALUES (?, ?, ?, 0, ?, CURRENT_TIMESTAMP(), ?,  CURRENT_TIMESTAMP(), 1)
     `,
-  getNotificationsList: `SELECT ${table_name}.*, u.first_name, u.last_name, f.flat_number FROM ${table_name} join users u on ${table_name}.user_id = u.user_id join tenancies t on ${table_name}.user_id = t.user_id join flats f on t.flats_id = f.flats_id WHERE ${table_name}.owner_id = ?`,
+  getNotificationsList: `SELECT ${table_name}.*, u.first_name, u.last_name, f.flat_number FROM ${table_name} join users u on ${table_name}.user_id = u.user_id join tenancies t on ${table_name}.user_id = t.user_id join flats f on t.flats_id = f.flats_id WHERE ${table_name}.owner_id = ? AND ${table_name}.created_by = ${table_name}.owner_id`,
   getNotificationDetails: `SELECT ${table_name}.*, u.first_name, u.last_name, f.flat_number FROM ${table_name} join users u on ${table_name}.user_id = u.user_id join tenancies t on ${table_name}.user_id = t.user_id join flats f on t.flats_id = f.flats_id WHERE owner_id = ? AND notification_id = ?`,
   markAsRead: `UPDATE ${table_name} SET status = 1, last_update_date = CURRENT_TIMESTAMP(), change_number = change_number + 1 WHERE owner_id = ? AND notification_id = ?`,
   unreadNotifications: `SELECT * FROM ${table_name} WHERE owner_id = ? AND status = 0`,
--- api/v1/tenant/notifications/queries/tenant-notifications.js
+++ api/v1/tenant/notifications/queries/tenant-notifications.js
@@ -1,7 +1,7 @@
 const table_name = "notifications";
 
 const queries = {
-  getNotificationsList: `select * from ${table_name} where user_id = ? AND created_by = user_id`,
+  getNotificationsList: `select * from ${table_name} where user_id = ? `,
   getNotificationDetails: `select * from ${table_name} where user_id = ? and notification_id = ?`,
   markAsRead: `UPDATE ${table_name} SET status = 1, last_update_date = CURRENT_TIMESTAMP(), change_number = change_number + 1 WHERE user_id = ? AND notification_id = ?`,
   unreadNotifications: `SELECT * FROM ${table_name} WHERE user_id = ? AND status = 0`,
